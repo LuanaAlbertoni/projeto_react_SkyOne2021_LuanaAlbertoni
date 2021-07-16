1dc81460e24875eb4e804bca2a6453cbe49c0dac
Luana Albertoni <65794490+LuanaAlbertoni@users.noreply.github.com>
2021-07-16 19:21:57 -0300

    Trocando a classe Square por uma Função

--- jogodavelha/src/index.js
+++ jogodavelha/src/index.js
@@ -2,23 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    // constructor (props) {
-    //     super(props);
-    //     this.state = {
-    //         value: null,
-    //     };
-    // }
+// class Square extends React.Component {
+//     constructor (props) {
+//         super(props);
+//         this.state = {
+//             value: null,
+//         };
+//     }
 
-    render () {
-        return (
-            <button className="square"
-            onClick={() => this.props.onClick()}>
-            {this.props.value}
-            </button>
-        );
-    }
+//     render () {
+//         return (
+//             <button className="square"
+//             onClick={() => this.props.onClick()}>
+//             {this.props.value}
+//             </button>
+//         );
+//     }
+// }
+
+function Square(props){
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
+
 
 class Board extends React.Component {
 
