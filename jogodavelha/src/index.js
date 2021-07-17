e4ef650d533d80c4673f112ec7ddd5ada84793e5
Luana Albertoni <65794490+LuanaAlbertoni@users.noreply.github.com>
2021-07-17 13:54:47 -0300

    Utilizando o método map do JavaScript fazemos um map sobre o history no componente Game. Criamos uma constante moves que irá retornar um botão que quando acionado volta para a jogada que ele representa. Para o botão funcionar definimos o método jumpTo para atualizar o stepNumber do botão. Colocamos o stepNumber: history.length para contar as jogadas e certificar que não fique mostrando sempre a mesma jogada. Modificamos o método render do Game para renderizar apenas a jogada selecionada de acordo com o stepNumber.Finalizando aqui o projeto

--- jogodavelha/src/index.js
+++ jogodavelha/src/index.js
@@ -69,12 +69,20 @@ class Game extends React.Component {
             history: [{
                 squares: Array(9).fill(null),
             }],
+            stepNumber: 0,
             xIsNext: true,
         };
     }
 
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) ===0,
+        });
+    }
+
     handleClick(i){
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -85,13 +93,14 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares,
             }]),
-        xIsNext: !this.state.xIsNext,
+            stepNumber: history.length,
+            xIsNext: !this.state.xIsNext,
         });
     }
 
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
         let status;
 
@@ -100,6 +109,17 @@ class Game extends React.Component {
         } else {
             status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         }
+
+        const moves = history.map((step, move) => {
+            const desc = move ?
+            'Go to move #' + move :
+            'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
 
         return (
             <div className="game">
@@ -111,7 +131,7 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/*To Do*/}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
