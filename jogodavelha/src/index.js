799dcfb298625ea161b090907b01bed43360b42a
Luana Albertoni <65794490+LuanaAlbertoni@users.noreply.github.com>
2021-07-16 22:20:05 -0300

    Criando uma função que irá mostar o vencedor quando o jogo for vencido. Criamos uma constante winner no render da Board  para mostrar o vencedor no status

--- jogodavelha/src/index.js
+++ jogodavelha/src/index.js
@@ -55,7 +55,14 @@ class Board extends React.Component {
     }
 
     render(){
-        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+        // const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         return (
             <div>
                 <div className="status">{status}</div>
@@ -101,3 +108,25 @@ ReactDOM.render (
     <Game />,
     document.getElementById('root')
 );
+
+
+function calculateWinner(squares) {
+
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
